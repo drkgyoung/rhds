734c5149d35b07a36d5b761d31b1b241cd707961
drkgyoung <k.young3@exeter.ac.uk>
2026-01-30 10:57:09 +0000

    updating to use config file

--- scripts/download-data.sh
+++ scripts/download-data.sh
@@ -2,6 +2,11 @@
 
 # set -e
 
+source config.env
+
+mkdir -p $datadir
+mkdir -p $resultsdir
+
 # for loop line by line in data/files.csv
 # Skip the first line
 {
@@ -9,6 +14,6 @@
     while IFS=, read -r url filename date time size
     do
         echo "Downloading $filename from $url"
-        curl -L $url/$filename -o $filename
+        curl -L $url/$filename -o $datadir/$filename
     done
 } < files.csv
